04dd377de2d58ee1fb5a3a48980da2f8a9e380a6
Adrian Macneil <adrian@crescendo.net.nz>
2012-07-27 17:02:24 +1000

    Update Ogone Directlink driver

--- libraries/merchant/merchant_ogone_directlink.php
+++ libraries/merchant/merchant_ogone_directlink.php
@@ -3,7 +3,7 @@
 /*
  * CI-Merchant Library
  *
- * Copyright (c) 2011-2012 Crescendo Multimedia Ltd
+ * Copyright (c) 2011-2012 Adrian Macneil
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,6 +28,7 @@
  * Merchant Ogone DirectLink Class
  *
  * Payment processing using Ogone DirectLink
+ * @link http://www.productivecomputing.com/docs/docs_library/FM_CreditCard/Ogone_DirectLink_EN.pdf
  */
 
 class Merchant_ogone_directlink extends Merchant_driver
@@ -35,79 +36,106 @@ class Merchant_ogone_directlink extends Merchant_driver
 	const PROCESS_URL = 'https://secure.ogone.com/ncol/prod/orderdirect.asp';
 	const PROCESS_URL_TEST = 'https://secure.ogone.com/ncol/test/orderdirect.asp';
 
-	public $required_fields = array('amount', 'card_no', 'card_name', 'exp_month', 'exp_year', 'csc', 'currency_code', 'reference');
-
-	public $settings = array(
-		'psp_id' => '',
-		'user_id' => '',
-		'password' => '',
-		'signature' => '',
-		'test_mode' => FALSE,
-	);
-
-	public $CI;
-
-	public function __construct()
+	public function default_settings()
 	{
-		$this->CI = get_instance();
+		return array(
+			'psp_id' => '',
+			'user_id' => '',
+			'password' => '',
+			'signature' => '',
+			'test_mode' => FALSE,
+		);
 	}
 
-	public function _process($params)
+	public function authorize()
 	{
-		$date_expiry = $params['exp_month'];
-		$date_expiry .= $params['exp_year'] % 100;
+		$request = $this->_build_authorize_or_purchase('RES');
+		$response = $this->post_request($this->_process_url(), $request);
+		return new Merchant_ogone_directlink_response($response);	
+	}
 
-		$request = array(
-			'PSPID' => $this->settings['psp_id'],
-			'USERID' => $this->settings['user_id'],
-			'PSWD' => $this->settings['password'],
-			'ORDERID' => $params['reference'],
-			'OPERATION' => 'SAL',
-			'AMOUNT' => round($params['amount'] * 100),
-			'CURRENCY' => $params['currency_code'],
-			'CARDNO' => $params['card_no'],
-			'ED' => $date_expiry,
-			'CN' => $params['card_name'],
-			'CVC' => $params['csc'],
-			'REMOTE_ADDR' => $this->CI->input->ip_address(),
-			'ECI' => 7,
-		);
+	public function purchase()
+	{
+		$request = $this->_build_authorize_or_purchase('SAL');
+		$response = $this->post_request($this->_process_url(), $request);
+		return new Merchant_ogone_directlink_response($response);
+	}
 
-		if ( ! empty($params['email'])) $request['EMAIL'] = $params['email'];
-		if ( ! empty($params['address'])) $request['OWNERADDRESS'] = $params['address'];
-		if ( ! empty($params['address2'])) $request['OWNERTOWN'] = $params['address2'];
-		if ( ! empty($params['postcode'])) $request['OWNERZIP'] = $params['postcode'];
-		if ( ! empty($params['city'])) $request['OWNERCTY'] = $params['city'];
-		if ( ! empty($params['phone'])) $request['OWNERTELNO'] = $params['phone'];
+	protected function _build_authorize_or_purchase($method)
+	{
+		$this->require_params('card_no', 'name', 'exp_month', 'exp_year', 'csc');
 
-		// generate secure SHA signature
+		$request = array();
+		$request['PSPID'] = $this->setting('psp_id');
+		$request['USERID'] = $this->setting('user_id');
+		$request['PSWD'] = $this->setting('password');
+		$request['OPERATION'] = $method;
+		$request['ORDERID'] = $this->param('order_id');
+		$request['AMOUNT'] = $this->amount_cents();
+		$request['CURRENCY'] = $this->param('currency');
+		$request['CARDNO'] = $this->param('card_no');
+		$request['ED'] = $this->param('exp_month').($this->param('exp_year') % 100);
+		$request['CN'] = $this->param('name');
+		$request['CVC'] = $this->param('csc');
+		$request['REMOTE_ADDR'] = $this->CI->input->ip_address();
+		$request['ECI'] = 7;
+		$request['EMAIL'] = $this->param('email');
+		$request['OWNERADDRESS'] = $this->param('address1');
+		$request['OWNERTOWN'] = $this->param('address2');
+		$request['OWNERZIP'] = $this->param('postcode');
+		$request['OWNERCTY'] = $this->param('city');
+		$request['OWNERTELNO'] = $this->param('phone');
+
+		$this->_sign_request($request);
+		return $request;
+	}
+
+	protected function _sign_request(&$request)
+	{
 		ksort($request);
 		$signature = '';
 		foreach ($request as $key => $value)
 		{
-			$signature .= $key.'='.$value.$this->settings['signature'];
+			$signature .= $key.'='.$value.$this->setting('signature');
 		}
 		$request['SHASIGN'] = sha1($signature);
+	}
 
-		$process_url = $this->settings['test_mode'] ? self::PROCESS_URL_TEST : self::PROCESS_URL;
-		$response = Merchant::curl_helper($process_url, $request);
-		if ( ! empty($response['error'])) return new Merchant_response('failed', $response['error']);
+	protected function _process_url()
+	{
+		return $this->setting('developer_mode') ? self::PROCESS_URL_TEST : self::PROCESS_URL;
+	}
+}
 
-		$xml = simplexml_load_string($response['data']);
+class Merchant_ogone_directlink_response extends Merchant_response
+{
+	protected $_response;
 
-		if (empty($xml['PAYID']))
+	public function __construct($response)
+	{
+		$this->_response = simplexml_load_string($response);
+		$this->_status = self::FAILED;
+
+		if (empty($this->_response['PAYID']))
 		{
-			return new Merchant_response('failed', 'invalid_response');
+			$this->_message = lang('merchant_invalid_response');
 		}
-		elseif ($xml['STATUS'] == '9')
+		elseif ((string)$this->_response['STATUS'] == '5')
 		{
-			return new Merchant_response('authorized', NULL, (string)$xml['PAYID'], (float)$xml['amount']);
+			$this->_status = self::AUTHORIZED;
+			$this->_reference = (string)$this->_response['PAYID'];
+		}
+		elseif ((string)$this->_response['STATUS'] == '9')
+		{
+			$this->_status = self::COMPLETE;
+			$this->_reference = (string)$this->_response['PAYID'];
 		}
 		else
 		{
-			return new Merchant_response('declined', (string)$xml['NCERRORPLUS'], (string)$xml['PAYID']);
+			$this->_message = (string)$xml['NCERRORPLUS'];
+			$this->_reference = (string)$this->_response['PAYID'];
 		}
 	}
 }
 
-/* End of file ./libraries/merchant/drivers/merchant_ogone_directlink.php */
+/* End of file ./libraries/merchant/merchant_ogone_directlink.php */
